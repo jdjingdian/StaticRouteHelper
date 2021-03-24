b1b5416e50ae0430d04c2459a21ecc0b6b201073
magicdian <jingdian@magicdian.cn>
2021-03-24 16:41:36 +0800

    Initial Commit

---
+++ StaticRouteHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StaticRouteHelper
+//
+//  Created by Derek Jing on 2021/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StaticRouteHelper/StaticRouteHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaticRouteHelperApp.swift
+//  StaticRouteHelper
+//
+//  Created by Derek Jing on 2021/3/24.
+//
+
+import SwiftUI
+
+@main
+struct StaticRouteHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
